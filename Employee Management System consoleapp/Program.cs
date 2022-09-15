a0dd5c5ee2a5c50a2099a76cc6c570e79ef5e60b
SWARNAYU MANDAL <swarnayumandal200500@gmail.com>
2022-09-16 00:39:35 +0530

    assi

---
+++ Employee Management System consoleapp/Program.cs
@@ -0,0 +1,30 @@
+﻿using Employee_Management_System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_System_consoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO LITWARE");
+            Console.WriteLine("-----------------");
+
+            Console.WriteLine("Enter EMPLOYEE NUMBER");
+            int no = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter EMPLOYEE'S NAME");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Enter EMPLOYEE'S SALARY");
+            double sal = Convert.ToDouble(Console.ReadLine());
+
+            Employee emp = new Employee(no, name, sal);
+            emp.CalculatedSalary();
+        }
+    }
+}
---
+++ Employee Management System consoleapp2/Program.cs
@@ -0,0 +1,62 @@
+﻿using Employee_Management_System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Management_System_consoleapp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            bool a = true;
+
+            Console.WriteLine("WELCOME TO LITWARE");
+            Console.WriteLine("-----------------------------------");
+            try
+            {
+                while (a)
+                {
+
+
+                    Console.WriteLine("Enter EMPLOYEE NUMBER");
+                    int no = Convert.ToInt32(Console.ReadLine());
+
+
+                    Console.WriteLine("Enter EMPLOYEE'S NAME");
+                    string name = Console.ReadLine();
+
+
+                    Console.WriteLine("Enter EMPLOYEE'S SALARY");
+                    double sal = Convert.ToDouble(Console.ReadLine());
+
+
+
+                    Employee emp = new Employee(no, name, sal);
+                    employees.Add(emp);
+
+                    emp.CalculatedSalary();
+
+                    Console.WriteLine("you have successfully registered");
+
+                    Console.WriteLine("enter 0 to exit");
+
+                    int i = Convert.ToInt32(Console.ReadLine());
+                    if (i == 0)
+                    {
+                        a = false;
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
